221f45b7a30ade67f33437e660e0e5522de12de5
hanpanai <work@verrok.ru>
2024-01-09 23:18:53 +0300

    feat: reading and filtering all files and send it to chan

--- cmd/root.go
+++ cmd/root.go
@@ -4,23 +4,86 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"os"
-
+	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"sync"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "hanpack",
 	Short: "Packing tool (initially for moodle plugins)",
-	Long: `CLI tool for recursively packing folders skipping odd directory, such as .git, .idea, node_modules, etc
+	Long: `CLI tool for recursively packing folders skipping odd directory, such as .git, .idea, node_modules, etc.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Be aware, it can take more time if you run it in large folders`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		f := viper.GetString("folder")
+
+		entries := make(chan string)
+
+		go processEntries(entries)
+
+		err := filepath.Walk(f, walk(entries))
+
+		if err != nil {
+			return
+		}
+
+		wg := &sync.WaitGroup{}
+
+		go func() {
+			wg.Wait()
+			close(entries)
+		}()
+
+	},
+}
+
+func zip(wg *sync.WaitGroup, s string) {
+	fmt.Println(s)
+	wg.Done()
+}
+
+func processEntries(ch <-chan string) {
+	for {
+		select {
+		case entry, ok := <-ch:
+			if !ok {
+				break
+			}
+			fmt.Println(entry)
+		}
+	}
+}
+
+func walk(ch chan<- string) filepath.WalkFunc {
+	return func(path string, info fs.FileInfo, err error) error {
+		if path == "." {
+			return nil
+		}
+
+		restrictedDir := []string{"node_modules", ".git", "vendor", ".idea"}
+
+		for i := range restrictedDir {
+			if restrictedDir[i] == path {
+				return filepath.SkipDir
+			}
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		ch <- path
+
+		return nil
+	}
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -33,13 +96,11 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	rootCmd.PersistentFlags().StringP("folder", "f", ".", "Choose directory to pack")
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hanpack.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	err := viper.BindPFlag("folder", rootCmd.PersistentFlags().Lookup("folder"))
+	if err != nil {
+		panic(err)
+	}
+	viper.SetDefault("folder", ".")
 }
